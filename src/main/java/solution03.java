71a2336b9d9899a664ad93f1a5fe9afde3ecd661
Jeanne Moore <mooresea910@gmail.com>
2021-09-01 19:12:38 -0400

    Add to Github

---
+++ src/main/java/solution03.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class solution03 {
+    /*
+     *     UCF   COP3330 Fall 2021 Assignment 1 Solution03
+     *     Copyright 2021 Sean Moore
+     */
+
+    public static void main(String[] args){
+        /*
+        - Create an application that prompts
+        the user for an input quote
+        it receives the quote as input
+        prompt for the author of the quote
+        it receives the author as input
+        the author and quote are returned in a
+        sentence via string concatenation
+         */
+        //Challenge: Modify to receive
+
+
+        Scanner input = new Scanner(System.in);
+        String author, quote;
+        System.out.printf("What is the quote? ");
+        quote = input.nextLine();
+        System.out.printf("Who said it? ");
+        author = input.nextLine();
+
+        System.out.println(author + " says, \"" + quote + "\"");
+
+
+
+
+
+
+    }
+}
