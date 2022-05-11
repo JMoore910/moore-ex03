a2c7ce808a99a10cbfed537effeca80d9527a368
J Moore <89619763+JMoore910@users.noreply.github.com>
2022-05-10 23:41:59 -0400

    Update Solution03.java

--- src/main/java/Solution03.java
+++ src/main/java/Solution03.java
@@ -1,14 +1,16 @@
+/*
+ *     UCF   COP3330 Fall 2021 Assignment 1 Solution03
+ *     Copyright 2021 Sean Moore
+ */
+
 import java.util.Scanner;
 
 public class Solution03 {
-    /*
-     *     UCF   COP3330 Fall 2021 Assignment 1 Solution03
-     *     Copyright 2021 Sean Moore
-     */
+
 
     public static void main(String[] args){
         /*
-        - Create an application that prompts
+        Create an application that prompts
         the user for an input quote
         it receives the quote as input
         prompt for the author of the quote
